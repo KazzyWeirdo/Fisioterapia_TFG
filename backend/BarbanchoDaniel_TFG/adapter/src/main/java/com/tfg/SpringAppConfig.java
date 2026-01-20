013e56f025a4791a39110e9d80ef1f9a33e3f0d6
Kazzy <93839905+KazzyWeirdo@users.noreply.github.com>
2026-01-20 13:19:01 +0100

    Swagger added

--- backend/BarbanchoDaniel_TFG/adapter/src/main/java/com/tfg/SpringAppConfig.java
+++ backend/BarbanchoDaniel_TFG/adapter/src/main/java/com/tfg/SpringAppConfig.java
@@ -1,7 +1,14 @@
 package com.tfg;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title= "fisioterapia API",
+version = "1.0",
+license = @License(name = "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International Public License", url = "https://creativecommons.org/licenses/by-nc-sa/4.0/")))
+
 public class SpringAppConfig {
 }
