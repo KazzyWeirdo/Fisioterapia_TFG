285fcbe7e5503459aba5d4bcee0a958fb2801d39
Kazzy <93839905+KazzyWeirdo@users.noreply.github.com>
2026-01-20 12:00:26 +0100

    Docker + application.properties

---
+++ backend/BarbanchoDaniel_TFG/adapter/src/main/java/com/tfg/SpringAppConfig.java
@@ -0,0 +1,7 @@
+package com.tfg;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppConfig {
+}
---
+++ backend/BarbanchoDaniel_TFG/bootstrap/src/main/java/com/tfg/bootstrap/Launcher.java
@@ -0,0 +1,11 @@
+package com.tfg.bootstrap;
+
+import com.tfg.SpringAppConfig;
+import org.springframework.boot.SpringApplication;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppConfig.class, args);
+    }
+}
