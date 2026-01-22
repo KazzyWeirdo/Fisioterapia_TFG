ad01b3b53e898d7430405d3007b6d561e5ae7ed4
Kazzy <93839905+KazzyWeirdo@users.noreply.github.com>
2026-01-22 09:16:59 +0100

    Green phase for PatientId

--- backend/Fisioterapia_TFG/model/src/main/java/com/tfg/patient/PatientId.java
+++ backend/Fisioterapia_TFG/model/src/main/java/com/tfg/patient/PatientId.java
@@ -1,4 +1,9 @@
 package com.tfg.patient;
 
 public record PatientId(int value) {
+    public PatientId {
+        if (value < 1) {
+            throw new IllegalArgumentException("'value' must be a positive integer");
+        }
+    }
 }
