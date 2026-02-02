ba92074e75251897c78f5ebe0b55277f1bf6dfeb
Kazzy <93839905+KazzyWeirdo@users.noreply.github.com>
2026-02-02 13:46:37 +0100

    Mapper and JpaEntity for PNI

---
+++ backend/Fisioterapia_TFG/adapter/src/main/java/com/tfg/adapter/out/persistence/pni/PniReportJpaEntity.java
@@ -0,0 +1,30 @@
+package com.tfg.adapter.out.persistence.pni;
+
+import com.tfg.adapter.out.persistence.patient.PatientJpaEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pni_reports")
+public class PniReportJpaEntity {
+    @Id
+    private int id;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private PatientJpaEntity patient;
+    @Column(nullable = false)
+    private java.time.LocalDate reportDate;
+    @Column(nullable = false)
+    private Double hoursAsleep;
+    private Double hrv;
+    private int stress;
+    @Column(nullable = false)
+    private int ntrs;
+    //@OneToMany(mappedBy = "pni_reports")
+    //private List<String> trainingLoads; TODO: Change to TrainingLoad class when created
+}
