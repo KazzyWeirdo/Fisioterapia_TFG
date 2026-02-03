bd40aedee959c99aa9181823dd85e4a7a65fb33e
KazzyWeirdo <93839905+KazzyWeirdo@users.noreply.github.com>
2026-02-03 09:28:39 +0100

    Indiba improvements (#3)
    * Changed IndibaSessionByPatiento to return only BeginDate
    
    * Canvis en el README.md
    
    * Changed a line of code in GetIndibaSessionService
    
    * Update README.md

--- backend/Fisioterapia_TFG/application/src/main/java/com/tfg/port/in/indiba/GetIndibaSessionFromPatientUseCase.java
+++ backend/Fisioterapia_TFG/application/src/main/java/com/tfg/port/in/indiba/GetIndibaSessionFromPatientUseCase.java
@@ -3,8 +3,9 @@ package com.tfg.port.in.indiba;
 import com.tfg.indiba.IndibaSession;
 import com.tfg.patient.PatientId;
 
+import java.util.Date;
 import java.util.List;
 
 public interface GetIndibaSessionFromPatientUseCase {
-    List<IndibaSession> getIndibaSessionsFromPatient(PatientId patientId);
+    List<Date> getIndibaSessionsFromPatient(PatientId patientId);
 }
